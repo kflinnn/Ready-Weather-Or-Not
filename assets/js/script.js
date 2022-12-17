529a3720380ae74e5ce23ce70f00d624e2df2bbf
kflinnn <kflinncoding@gmail.com>
2022-12-17 10:46:36 -0500

    updated HTML, JS, added a lot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
-var apiKey = da7faa56794c42ea4ad50f04bea2b9cd
-//var urlRequest = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+//Global Variables
+var userFormEl = $("#user-form");
+var searchButton = $("#search-button");
+var resultsFormEl = $("#results-card");
+var forecastContainerEl = $("#forecast-container");
 
+
+var apiKey = "da7faa56794c42ea4ad50f04bea2b9cd"
+//var urlRequest = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+var city = $("#city-name");
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+//Event Listener to make button work
+searchButton.on("click", function(){
+    var userInput = document.querySelector("#city-name").value;
+localStorage.setItem("city", userInput);
+console.log(userInput);
+});
+
+//Function to fetch API
+// fetch(queryURL)
+// .then(function(response){
+//     console.log(response);
+//     return response.json();
+// })
+// .then(function(data){
+//     console.log(data);
+//     for (var i = 0; i < data.length; i++){
+        
+//     }
+// })
