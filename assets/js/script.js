e835d571c2119ed7a5ab0d08594bc71dc1e0a0ea
kflinnn <kflinncoding@gmail.com>
2022-12-18 16:53:22 -0500

    stored user input in a variable, started local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,25 @@
 //Global Variables
 var userFormEl = $("#user-form");
-var searchButton = $("#search-button");
+var searchButton = $("#search-btn");
 var resultsFormEl = $("#results-card");
 var forecastContainerEl = $("#forecast-container");
 
 
 var apiKey = "da7faa56794c42ea4ad50f04bea2b9cd"
 //var urlRequest = api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-var city = $("#city-name");
+var city = document.getElementById("user-input").value;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+console.log(queryURL)
 
 //Event Listener to make button work
 searchButton.on("click", function(){
-    var userInput = document.querySelector("#city-name").value;
-localStorage.setItem("city", userInput);
-console.log(userInput);
+    var city = document.getElementById("user-input").value;
+    console.log(city);
+   localStorage.setItem("city", city);
+
 });
+
 
 //Function to fetch API
 // fetch(queryURL)
